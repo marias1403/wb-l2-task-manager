48c7921c2db6879348c28486e9cc25decb828fd0
Maria Arkhipova <marias1403@yandex.ru>
2023-10-20 16:59:06 +0900

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="page">
+      <div className="page__container">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
