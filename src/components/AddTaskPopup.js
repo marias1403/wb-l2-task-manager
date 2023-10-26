d42bb10f371d5af83e475817a168d7dafdae892e
Maria Arkhipova <marias1403@yandex.ru>
2023-10-26 19:53:34 +0900

    refactor: AddTaskPopup

--- src/components/AddTaskPopup.js
+++ src/components/AddTaskPopup.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import closeIcon from '../assets/img/close-icon.svg';
+import Popup from './Popup';
 
 function AddTaskPopup(props) {
   const [title, setTitle] = useState('');
   const [deadline, setDeadline] = useState('');
-  const isOpen = props.isOpen ? 'popup_opened' : '';
 
   function handleChangeTitle(e) {
     setTitle(e.target.value);
@@ -30,45 +29,36 @@ function AddTaskPopup(props) {
     });
   }
 
-  return (
-    <div onMouseDown={(e) => {
-      if (e.target === e.currentTarget) {
-        props.onClose()
-      }
-    }} className={`popup ${isOpen}`}>
-      <div className='popup__container'>
-        <button onMouseDown={props.onClose} className='button'>
-          <img className='popup__close-icon' src={closeIcon} alt='Закрыть' />
-          <span>Закрыть</span>
-        </button>
-        <h2 className='popup__title'>Новая задача</h2>
-        <form onSubmit={handleSubmit} className='popup__form'>
-          <label className='popup__label' htmlFor='task'>Название</label>
-          <input
-            id='task'
-            type='text'
-            minLength='2'
-            maxLength='200'
-            className='popup__input'
-            placeholder=''
-            required
-            value={title || ''}
-            onChange={handleChangeTitle}
-          />
-          <label className='popup__label' htmlFor='datetime'>Дедлайн</label>
-          <input
-            id='datetime'
-            type='datetime-local'
-            className='popup__input'
-            required
-            value={deadline || ''}
-            onChange={handleChangeDate}
-          />
-          <button className='popup__submit-button' type='submit'>Добавить</button>
-        </form>
-      </div>
-    </div>
-  );
+  return <Popup
+    isOpen={props.isOpen}
+    name={'addTask'}
+    title={'Новая задача'}
+    onClose={props.onClose}
+    onSubmit={handleSubmit}
+    buttonText={'Добавить'}
+  >
+    <label className='popup__label' htmlFor='task'>Название</label>
+    <input
+      id='task'
+      type='text'
+      minLength='2'
+      maxLength='200'
+      className='popup__input'
+      placeholder=''
+      required
+      value={title || ''}
+      onChange={handleChangeTitle}
+    />
+    <label className='popup__label' htmlFor='datetime'>Дедлайн</label>
+    <input
+      id='datetime'
+      type='datetime-local'
+      className='popup__input'
+      required
+      value={deadline || ''}
+      onChange={handleChangeDate}
+    />
+  </Popup>
 }
 
 export default AddTaskPopup;
